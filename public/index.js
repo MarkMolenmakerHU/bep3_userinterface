16751054753d54c440d0eb37ba394265c81ce0dd
markmolenmaker <mark.molenmaker@student.hu.nl>
2021-12-08 16:20:31 +0100

    Update, many stuff

--- public/index.js
+++ public/index.js
@@ -1,7 +1,9 @@
+const URL = 'http://localhost:8080/inventory'
 const main_container = document.querySelector('#main-container');
+const form = document.querySelector('#item-form');
 
 function buyStockRequest(id) {
-    fetch(`http://localhost:8080/inventory/${id}/stock`, {
+    fetch(`${URL}/${id}/stock`, {
         method: 'POST'
     })
         .catch(err => console.error(err))
@@ -10,7 +12,7 @@ function buyStockRequest(id) {
 
 function getAllItemsRequest() {
     main_container.innerHTML = '';
-    fetch('http://localhost:8080/inventory', {
+    fetch(URL, {
         method: 'GET'
     })
         .then(res => res.json())
@@ -18,7 +20,7 @@ function getAllItemsRequest() {
             json.forEach(item => {
                 const item_element = document.querySelector('#item-template')
                     .content.cloneNode(true);
-                item_element.querySelector('.id').textContent = item['id'];
+                item_element.querySelector('.id').value = item['id'];
                 item_element.querySelector('.productName').textContent = item['productName'];
                 item_element.querySelector('.sellPrice').textContent = item['sellPrice'];
                 item_element.querySelector('.sellCapacity').textContent = item['sellCapacity'];
@@ -35,6 +37,84 @@ function getAllItemsRequest() {
         .catch(err => console.error(err))
 }
 
+function get_form() {
+    const id = form.querySelector('#item-form-id').value;
+    fetch(`${URL}/${id}`, {
+        method: 'GET'
+    })
+        .then(res => res.json())
+        .then(json => {
+            fill_form(json);
+        })
+        .catch(err => console.error(err))
+}
+
+function delete_form() {
+    const id = form.querySelector('#item-form-id').value;
+    fetch(`${URL}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(() => {
+            form.reset();
+            getAllItemsRequest();
+        })
+        .catch(err => console.error(err))
+}
+
+function register_form() {
+    const data = new FormData(form);
+    fetch(URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(Object.fromEntries(data.entries()))
+    })
+        .then(res => res.json())
+        .then(json => {
+            fill_form(json);
+            getAllItemsRequest();
+        })
+        .catch(err => console.error(err))
+}
+
+function update_form() {
+    const data = new FormData(form);
+    const id = form.querySelector('#item-form-id').value;
+    fetch(`${URL}/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(Object.fromEntries(data.entries()))
+    })
+        .then(res => res.json())
+        .then(json => {
+            fill_form(json);
+            getAllItemsRequest();
+        })
+        .catch(err => console.error(err))
+}
+
+function fill_form(item) {
+    // Clear all form items
+    form.reset();
+
+    form.querySelector('#item-form-id').value = item['id'];
+    form.querySelector('#item-form-productName').value = item['productName'];
+    form.querySelector('#item-form-stock').value = item['stock'];
+    form.querySelector('#item-form-unit').value = item['unit'];
+    form.querySelector('#item-form-sellPrice').value = item['sellPrice'];
+    form.querySelector('#item-form-sellCapacity').value = item['sellCapacity'];
+    form.querySelector('#item-form-capacity').value = item['capacity'];
+    form.querySelector('#item-form-purchaseCapacity').value = item['purchaseCapacity'];
+    form.querySelector('#item-form-purchasePrice').value = item['purchasePrice'];
+}
+
+// Setup update button
 document.querySelector('#getAllItems-button').addEventListener('click', () => getAllItemsRequest())
 
+// Setup form button
+document.querySelector('#get-form-button').addEventListener('click', () => get_form())
+document.querySelector('#update-form-button').addEventListener('click', () => update_form())
+document.querySelector('#register-form-button').addEventListener('click', () => register_form())
+document.querySelector('#delete-form-button').addEventListener('click', () => delete_form())
+
+// Get all items after loading everything
 getAllItemsRequest();
